c781f74570b6764548a4b4dd89ec659420175f7a
William McGonagle <wpmcgonagle@gmail.com>
2021-12-11 21:45:59 -0500

    🧪 Still Trying to Figure out the Site

--- docs/home.92a06916.js
+++ docs/home.92a06916.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/main.sass":[function(require,module,exports) {
+},{"./bundle-url":"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/home.sass":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.22a94128.js.map
+//# sourceMappingURL=/home.92a06916.js.map

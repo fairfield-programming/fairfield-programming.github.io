074d3bc3f8f992ccbf2d3517471852682546dadc
William McGonagle <wpmcgonagle@gmail.com>
2021-12-24 10:08:43 -0500

    🎨 Finished Login Page

--- auth/login/index.ts
+++ auth/login/index.ts
@@ -1,5 +1,43 @@
+function validateForm(username, password) {
+
+
+
+    return false;
+
+}
+
 window.onload = function () {
 
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+
+    document.getElementById('form').onsubmit = () => { return validateForm(username, password) };
+
+    // Make Hide/ Show Buttons
+    var invisibleButton = document.getElementById('buttonInvisible');
+    var visibleButton = document.getElementById('buttonVisible');
+
+    invisibleButton.style.display = 'none';
+
+    visibleButton.onclick = () => {
+
+        invisibleButton.style.display = 'block';
+        visibleButton.style.display = 'none';
+        
+        password.setAttribute('type', 'text');
+
+    };
+
+    invisibleButton.onclick = () => {
+
+        invisibleButton.style.display = 'none';
+        visibleButton.style.display = 'block';
+
+        password.setAttribute('type', 'password');
+
+    };
+
+    // Get the Jokes
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", 'https://fairfield-programming.herokuapp.com/joke', true);
     xmlHttp.send(null);
--- docs/auth/login/index.js
+++ docs/auth/login/index.js
@@ -117,47 +117,74 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"auth/login/index.ts":[function(require,module,exports) {
-function validateForm(username, password) {
-  return false;
+})({"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
 }
 
-window.onload = function () {
-  var username = document.getElementById('username');
-  var password = document.getElementById('password');
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
 
-  document.getElementById('form').onsubmit = function () {
-    return validateForm(username, password);
-  }; // Make Hide/ Show Buttons
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
 
+  return '/';
+}
 
-  var invisibleButton = document.getElementById('buttonInvisible');
-  var visibleButton = document.getElementById('buttonVisible');
-  invisibleButton.style.display = 'none';
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
 
-  visibleButton.onclick = function () {
-    invisibleButton.style.display = 'block';
-    visibleButton.style.display = 'none';
-    password.setAttribute('type', 'text');
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
   };
 
-  invisibleButton.onclick = function () {
-    invisibleButton.style.display = 'none';
-    visibleButton.style.display = 'block';
-    password.setAttribute('type', 'password');
-  }; // Get the Jokes
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
 
+var cssTimeout = null;
 
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", 'https://fairfield-programming.herokuapp.com/joke', true);
-  xmlHttp.send(null);
-  console.log(xmlHttp);
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
 
-  xmlHttp.onload = function (res) {
-    document.getElementById('joke').innerHTML = xmlHttp.response;
-  };
-};
-},{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -361,5 +388,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","auth/login/index.ts"], null)
-//# sourceMappingURL=/login.6883b22f.js.map
+},{}]},{},["../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/auth/login/index.js.map

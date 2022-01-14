001b82a342dfeace5e3bf43bd94e27dfbdf8c4db
William McGonagle <wpmcgonagle@gmail.com>
2022-01-13 21:13:21 -0500

    ✨ Changed Header

---
+++ includes/header.ts
@@ -0,0 +1,12 @@
+// const Cookies = require('js-cookie');
+
+var loggedOut = document.getElementById("loggedOut");
+var loggedIn = document.getElementById("loggedIn");
+
+if (false) {
+    loggedOut.style.display = "none";
+    loggedIn.style.display = "block";
+} else {
+    loggedOut.style.display = "block";
+    loggedIn.style.display = "none";
+}

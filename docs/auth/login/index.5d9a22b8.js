112eb70d32eb98d419cf9e1d2d41384489738ea3
William McGonagle <wpmcgonagle@gmail.com>
2022-03-09 22:32:57 -0500

    Testing

---
+++ docs/auth/login/index.5d9a22b8.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("login-form");
+loginForm.onsubmit = ()=>{
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    let request = new Request("https://fairfield-programming.herokuapp.com/user/login", {
+        method: "POST",
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+    fetch(request).then((data)=>{
+        console.log(data);
+    }).catch((error)=>{
+        console.log(error);
+    });
+    return false;
+};
+
+//# sourceMappingURL=index.5d9a22b8.js.map
---
+++ src/auth/login/login.js
@@ -0,0 +1,28 @@
+const loginForm = document.getElementById("login-form");
+
+loginForm.onsubmit = () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    let request = new Request("https://fairfield-programming.herokuapp.com/user/login", {
+        method: "POST",
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+
+    fetch(request).then((data) => {
+
+        console.log(data);
+
+    }).catch((error) => {
+
+        console.log(error);
+
+    });
+
+    return false;
+
+};

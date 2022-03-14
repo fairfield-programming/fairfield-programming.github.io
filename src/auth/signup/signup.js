663b94d0e3520e6f94e61a6aaa76f8502b277716
MassiGy <ghernaoutmassi@gmail.com>
2022-03-14 20:46:52 +0100

    Fixed DOM Input Element Access

--- src/auth/signup/signup.js
+++ src/auth/signup/signup.js
@@ -2,7 +2,7 @@ const signupForm = document.getElementById("signup-form");
 
 signupForm.onsubmit = () => {
 
-    const username = document.getElementById("password").username;
+    const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 

17361a1de8d87352d3904487a6f3d4d7b1f573e4
William McGonagle <wpmcgonagle@gmail.com>
2022-04-02 16:11:18 -0400

    Added Cloudflare Analytics

--- docs/index.e843d7f0.js
+++
@@ -1,7 +0,0 @@
-// const Cookies = require('js-cookie');
-var loggedOut = document.getElementById("loggedOut");
-var loggedIn = document.getElementById("loggedIn");
-loggedOut.style.display = "block";
-loggedIn.style.display = "none";
-
-//# sourceMappingURL=index.e843d7f0.js.map

4a086fe33bf20ca425b0c5eb54324ea0278431be
William McGonagle <wpmcgonagle@gmail.com>
2022-04-02 18:42:33 -0400

    Fixed Auth

--- docs/auth/login/index.5d9a22b8.js
+++ docs/auth/login/index.5d9a22b8.js
@@ -1,10 +1,7 @@
 const loginForm = document.getElementById("login-form");
-
-loginForm.onsubmit = () => {
-
+loginForm.onsubmit = ()=>{
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-
     let request = new Request("https://fairfield-programming.herokuapp.com/user/login", {
         method: "POST",
         body: JSON.stringify({
@@ -16,35 +13,20 @@ loginForm.onsubmit = () => {
             "Content-Type": "application/json"
         })
     });
-
-    fetch(request).then((data) => {
-
+    fetch(request).then((data)=>{
         if (data.status == 200) {
-
             Cookies.set('token', data.json().token);
             window.location.href = "/dashboard";
-
-        } else {
-
-            displayWarning("Incorrect Email or Password.");
-
-        }
-
-    }).catch((error) => {
-
+        } else displayWarning("Incorrect Email or Password.");
+    }).catch((error)=>{
         displayWarning("Internal Error- Try Reloading the Page.");
-
     });
-
     return false;
-
 };
-
 function displayWarning(warningText) {
-
     var warningBox = document.getElementById("warningBox");
-
     warningBox.innerHTML = warningText;
     warningBox.style.display = "block";
-
 }
+
+//# sourceMappingURL=index.5d9a22b8.js.map

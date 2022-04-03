8189aa6ea3a41d35ce27d3b97bb8b60a7c601288
William McGonagle <wpmcgonagle@gmail.com>
2022-04-02 21:54:34 -0400

    Pushed It

--- docs/question/index.4a28e800.js
+++ docs/question/index.4a28e800.js
@@ -3,55 +3,36 @@ const questionName = document.getElementById("questionName");
 const questionIcon = document.getElementById("questionIcon");
 const questionUsername = document.getElementById("questionUsername");
 const answerParent = document.getElementById("answerParent");
-
-fetch("https://fpa-questions.herokuapp.com/question/1").then((response) => {
-
+fetch("https://fpa-questions.herokuapp.com/question/1").then((response)=>{
     if (response.status != 200) window.location.href = "/";
-
     return response;
-
-}).then(response => response.json()).then((data) => {
-
+}).then((response)=>response.json()
+).then((data)=>{
     title.innerHTML = data.body;
-
-    data.answers.forEach(element => {
-        
-       addAnswer(element, ) 
-
+    data.answers.forEach((element)=>{
+        addAnswer(element);
     });
-
     console.log(data);
-
-})
-
-function addAnswer(data, parent) {
-
+});
+function addAnswer(data1, parent) {
     const container = document.createElement("div");
     const paragraph = document.createElement("p");
-
-    fetch(`https://fairfield-programming.herokuapp.com/user/${data.user}`).then(response => response.json()).then((data) => {
-
+    fetch(`https://fairfield-programming.herokuapp.com/user/${data1.user}`).then((response)=>response.json()
+    ).then((data)=>{
         const userContainer = document.createElement("div");
         const userIcon = document.createElement("img");
-
         userContainer.className = "d-flex text-muted pt-3";
-
         userContainer.append(userIcon);
-
         userIcon.src = data.profilePicture;
-
         container.append(userContainer);
-
-    })
-
+    });
     container.className = "bg-body border my-3 p-3 rounded";
-
-    paragraph.innerHTML = data.body;
-
+    paragraph.innerHTML = data1.body;
     container.append(paragraph);
-
     parent.append(container);
-
 }
+addAnswer({
+    body: "This is a test"
+}, answerParent);
 
-addAnswer({ body: "This is a test" }, answerParent)
+//# sourceMappingURL=index.4a28e800.js.map

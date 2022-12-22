5052bdfd02393b11d14a9a90f78d19fb7fefd37a
William McGonagle <wpmcgonagle@gmail.com>
2022-12-21 22:47:20 -0500

    Removed Docs

--- docs/index.67b28f2b.js
+++
@@ -1,7 +0,0 @@
-// const Cookies = require('js-cookie');
-var loggedOut = document.getElementById("loggedOut");
-var loggedIn = document.getElementById("loggedIn");
-loggedOut.style.display = "block";
-loggedIn.style.display = "none";
-
-//# sourceMappingURL=index.67b28f2b.js.map

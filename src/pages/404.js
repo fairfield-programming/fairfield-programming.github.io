4f690ce51ef3e145ef3e1aef1ac2a4fe5a780c81
vihaanrajesh <105464310+vihaanrajesh@users.noreply.github.com>
2022-12-30 19:54:06 -0500

    Adds 404 page [Finishes #48]

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,19 @@ import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex justify-center">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+        <div className="flex justify-center font-light text-6xl text-gray-500 text-opacity-75">
+          404
+        </div>
+        <div className="flex justify-center font-medium text-lg my-2 text-gray-800">The page you're looking for is not found.</div>
+        <hr />
+        <div className="px-6 py-4">
+          Make sure the address is correct. If you think this is a mistake,
+          <a href="/contact" className="ml-1 text-orange-400">contact us</a>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
